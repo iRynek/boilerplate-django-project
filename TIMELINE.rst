d2bb167ec560efa4e567104d0acbf8f8e7ffc0ea
Leszek Piątek <lpiatek@gmail.com>
2013-05-29 19:05:38 +0200

    initial commit, docs needs to be changed

---
+++ TIMELINE.rst
@@ -0,0 +1,10 @@
+boilerplate-django-project future
+=========================
+
+#. update problem?
+#. django 'best coding' practicies (separate project?), coding rules
+#. source dir (for desigers to backup their work?)
+#. sandbox dir (For code that is not used now anymore but could be used in a project in a near future. For keeping code samples? Maybe it should be kept in separate branches in VCS?)
+#. fabric scripts for dev server
+#. CSS/JS/HTML starting templates/libs?
+#. script/command to remove unecessary things like (README.rst's, ect.)
---
+++ env.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+# setup pythonpath - add ./lib before any others paths
+# overwrite django and other shared python libs
+# it's one of 'little ugly hack' that make things works
+__root = os.path.dirname(os.path.abspath(__file__))
+__path = lambda *a: os.path.join(__root, *a)
+sys.path.insert(0, __path(''))
+if os.path.exists(__path('lib')):
+    sys.path.insert(0, __path('lib'))
+del __root, __path
+
+
+def setup(settings_module_name=None):
+    """
+    Simple setup snippet that makes easy to create
+    fast sandbox to try new things.
+
+    :param settings_module_name: name of settings module eg:
+         "project.setting"
+
+    Usage:
+    >>> import env
+    >>> env.setup()
+    >>> # from now on paths are setup, and django is configured
+    >>> # you can use it in separate "sandbox" script just to check
+    >>> # things really quick
+    """
+    settings_module_name = settings_module_name or 'apps.settings'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module_name)
+
+
+# You can use this module as
+# WSGIScriptAlias / "path/to/env.py"
+def application(environ, start_response):
+    setup()
+    from django.core.handlers.wsgi import WSGIHandler
+    return WSGIHandler()(environ, start_response)
+
