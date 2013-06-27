740cd6cb1f3971707e6667da22a4b0c83250d46a
Leszek Piątek <lpiatek@gmail.com>
2013-06-27 17:19:11 +0200

    some cleanup + added bootstrap v2.3.2

--- apps/settings/local-sample-dev.py
+++ apps/settings/local-sample-dev.py
@@ -16,7 +16,7 @@ SECRET_KEY = ''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': projectpath('dev.db'),
+        'NAME': projectpath('') + 'dev.db',
     }
 }
 
